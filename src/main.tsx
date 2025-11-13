59bb40a7a98a6eb8439f35588503a5ca29f02d6b
William <williambosworth420@gmail.com>
2025-11-13 15:12:37 +0100

    William: Setup navbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Hero from './pages/Hero'
+import NavBar from './pages/NavBar'
 import './main.css'
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
-      <Hero />
+      <NavBar />
     </React.StrictMode>
   );
 } else {
